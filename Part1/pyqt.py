06bd29ef1b71dc718dbc480220ee80a3cb8ab21b
mhsyzdni <yzdnimahsa@gmail.com>
2022-07-14 21:57:41 +0430

    Convert QtDesigner into .py & Load QtDesigner

--- Part1/pyqt.py
+++ Part1/pyqt.py
@@ -1,7 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QWidget
 import sys
+from PyQt6 import uic
+
+class UiLoad(QWidget):
+    def __init__(self):
+        super.__init__(self)
+        uic.loadUi("MonitoringUi.ui", self)
+
 
 app = QApplication(sys.argv)
-window=QWidget()
+window = QWidget()
 window.show()
 sys.exit(app.exec())
